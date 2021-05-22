69c05efc988432e1c443ac2fb06f8789ff435119
BaltiAhmed <ahmedbalti06@gmail.com>
2021-05-22 12:52:12 +0100

    ahmed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Authcontext } from "./context/auth-context";
+import { UserAuth } from "./hooks/Auth-houks";
+import { Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import NavLogin from "./components/nav-login";
+import MenuDrawer from './components/menu'
 
 function App() {
+  const { user, token, login, logout } = UserAuth();
+  let routes;
+  if (token) {
+    routes = (
+      <React.Fragment>
+        <Route path="/" exact component={Home} />
+      </React.Fragment>
+    );
+  } else {
+    routes = (
+      <React.Fragment>
+        <Route path="/" exact component={Login} />
+      </React.Fragment>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Authcontext.Provider
+        value={{ user: user, token: token, login: login, logout: logout }}
+      >
+        <BrowserRouter>
+          {!token ? <NavLogin /> : <MenuDrawer content={routes} />}
+
+          {!token && routes}
+        </BrowserRouter>
+      </Authcontext.Provider>
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+
+
+const Home = (props)=>{
+    return(
+        <div>
+            <h1>Home page</h1>
+        </div>
+    )
+}
+
+export default Home
